a23e36c4ea8015af0d34b3a98a9cb52e145b41d0
abdullahadaileh <abdullahadaileh957@gmail.com>
2024-09-19 15:12:15 +0300

    axios

--- src/App3.js
+++ src/App3.js
@@ -1,5 +1,69 @@
+
+// fetch
+
+// import React, { useState, useEffect } from 'react';
+// import './App.css';  
+
+// const DataFetchingComponent = () => {
+//   const [data, setData] = useState([]); 
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+
+//         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch data'); 
+//         }
+
+//         const result = await response.json();
+//         setData(result); 
+//         setLoading(false); 
+//       } catch (error) {
+//         setError(error.message); 
+//         setLoading(false); 
+//       }
+//     };
+
+//     fetchData();
+//   }, []); 
+  
+
+//   if (loading) {
+//     return <p>Loading data...</p>;
+//   }
+//   if (error) {
+//     return <p>Error fetching data: {error}</p>;
+//   }
+
+
+//   return (
+//     <div style={{ padding: '20px', textAlign: 'center' }}>
+//       <h2>Fetched Posts</h2>
+//       <ul style={{ listStyleType: 'none', padding: 0 }}>
+//         {data.slice(0, 10).map((post) => ( 
+//           <li key={post.id} style={{ margin: '10px 0', borderBottom: '1px solid #ddd' }}>
+//             <h3>{post.title}</h3>
+//             <p>{post.body}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+// export default DataFetchingComponent;
+
+
+
+// axios
+
 import React, { useState, useEffect } from 'react';
 import './App.css';  
+import axios from 'axios';
 
 const DataFetchingComponent = () => {
   const [data, setData] = useState([]); 
@@ -11,14 +75,10 @@ const DataFetchingComponent = () => {
     const fetchData = async () => {
       try {
 
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch data'); 
-        }
 
-        const result = await response.json();
-        setData(result); 
+        setData(response.data); 
         setLoading(false); 
       } catch (error) {
         setError(error.message); 
